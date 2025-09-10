dd9592547c74a62c20e101db6559c576a7eee992
Alexey Tihomirov <alexey.tihomirov7@gmail.com>
2025-09-10 23:27:52 +0300

    added seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,24 @@
 import { defineConfig } from 'astro/config';
-
 import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [solidJs()],
-  output: "static",
+  site: 'https://tkani-nortex.ru',
+  trailingSlash: 'never',
+  build: {
+    format: 'directory'
+  },
+  compressHTML: true,
+  vite: {
+    build: {
+      minify: 'terser',
+      terserOptions: {
+        compress: {
+          drop_console: true,
+          drop_debugger: true
+        }
+      }
+    }
+  }
 });
