89a86d56953672771cfa1853237bd5a49851b102
Alexey Tihomirov <alexey.tihomirov7@gmail.com>
2025-10-28 23:07:14 +0200

    updated URL

--- src/lib/pocketbase.js
+++ src/lib/pocketbase.js
@@ -1,19 +1,19 @@
 // lib/pocketbase.js
-import PocketBase from 'pocketbase';
+import PocketBase from "pocketbase";
 
-const url = 'https://eliza-norteks.pockethost.io/';
+export const url = "http://80.87.198.50:8090";
 export const client = new PocketBase(url);
 
 export const getImageUrl = (item) => {
-	if (!item.image) return '';
-	
-	const collectionId = item.collectionId;
-	const fileId = item.id;
-	const fileName = item.image;
-	return `${url}/api/files/${collectionId}/${fileId}/${fileName}`;
+  if (!item.image) return "";
+
+  const collectionId = item.collectionId;
+  const fileId = item.id;
+  const fileName = item.image;
+  return `${url}/api/files/${collectionId}/${fileId}/${fileName}`;
 };
 
 export async function fetchItems() {
-	const resultList = await client.collection('types').getList(1, 50)
-	return resultList.items;
+  const resultList = await client.collection("types").getList(1, 50);
+  return resultList.items;
 }
