acf9ca43dd8f63d5f42bfad15a126634800b2d2c
Alexey <alexey.tihomirov7@gmail.com>
2025-11-11 20:55:53 +0200

    updated config, fixed links

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,26 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [solidJs()],
-  site: 'https://tkani-nortex.ru',
-  trailingSlash: 'never',
+  site: "https://tkani-nortex.ru",
+  trailingSlash: "never",
   build: {
-    format: 'directory'
+    format: "directory",
+    assets: "_astro",
   },
-  compressHTML: true
+  compressHTML: true,
+  vite: {
+    build: {
+      cssCodeSplit: true,
+      rollupOptions: {
+        output: {
+          entryFileNames: "_astro/[name].[hash].js",
+          chunkFileNames: "_astro/[name].[hash].js",
+          assetFileNames: "_astro/[name].[hash][extname]",
+        },
+      },
+    },
+  },
 });
